e40f28717d10e14faa7eec75649e724b0de4d323
Allyson Wesman <allysonwesman@gmail.com>
2018-01-04 21:15:25 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
-def valid_move?(board, move)
-  if move > 8 || move < 0 || position_taken?
+def valid_move?(board, index)
+  if index > 8 || index < 0 || position_taken?(board, index)
     return false
   else
     return true
